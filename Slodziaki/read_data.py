3f66cfce8073c36b83a6cd5bee80a6470f925a46
Aleksander Nowak <olek.nowak.20@gmail.com>
2024-05-12 19:52:01 +0200

    We now read data from csv into python list

--- Slodziaki/read_data.py
+++ Slodziaki/read_data.py
@@ -1,2 +1,30 @@
 import csv
-# each sequence needs to be one element in the dataset
+import sys
+import numpy as np
+
+# each sequence needs to be one element in the returned list
+def read_csv(path):
+    data = []
+    with open(path, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        prev_row_name = ""
+        current_sequence = []
+        for row in reader:
+            if prev_row_name == row[0]:
+                current_sequence.append(row[1])
+            else:
+                data.append(current_sequence)
+                current_sequence = [row[1]] 
+                prev_row_name = row[0]
+    return data[2:]
+
+
+def main(args):
+    og_data = []
+    og_path = "/home/agentolek/AI/Bootcamp-mini-hackathon-2024/dane/zadanie_1/znaki-sekwencje-20160604-original.csv"
+    og_data = read_csv(og_path)
+
+    print(og_data[0])
+
+if __name__ == "__main__":
+    main(sys.argv)
